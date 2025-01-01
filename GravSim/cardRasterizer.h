00becf91eadf7391e8432c0eeaac6c5b2a8a4cea
EvertonTGV2006 <gareth.everton@outlook.com>
2025-01-01 23:23:18 +0000

    Visual Studio bugfix

---
+++ GravSim/cardRasterizer.h
@@ -0,0 +1,179 @@
+#pragma once
+
+#include <vulkan/vulkan.h>
+
+#include <array>
+#include <vector>
+#include <chrono>
+
+
+#define CARD_COUNT 52
+
+#include "structs.h"
+#include "player.h"
+#include "cardEngine.h"
+
+
+struct CardInit {
+	VkDevice device;
+	VkDescriptorPool descriptorPool;
+	VkRenderPass  renderPass;
+	VkSampleCountFlagBits msaaSamples;
+
+	VkPhysicalDeviceMemoryProperties memProperties;
+	std::array<std::vector<char>*, 2> shaderCode;
+
+	PlayerObject* player;
+
+	GameTablePtr gameTable;
+	
+};
+
+struct CardPushConstants {
+	glm::mat4 viewMat;
+	glm::mat4 viewPojectionMatrix;
+	glm::vec4 pos;
+	glm::vec4 dir;
+	glm::vec4 colour;
+	glm::vec4 eyePos;
+};
+struct CardDataConstant {
+	glm::mat4 cardMat;
+};
+
+
+
+class CardRasterizer {
+public:
+	void initCard_A(CardInit);
+	void initCard_B();
+	StatusLogger* stat;
+
+	void initMemory(std::array<MemInit, 4>);
+	
+	void getMemoryRequirements(std::vector<MemoryDetails>*, std::vector<uint16_t>*);
+
+	void cleanup();
+
+	uint32_t playerIndex = 0;
+
+	static const uint32_t MAX_STRING_LENGTH = 4096;
+
+	void drawElements(VkCommandBuffer, uint32_t, bool, glm::mat4, glm::mat4);
+
+	static const uint32_t FRAMES_IN_FLIGHT = 3;
+
+	void initBufferData_A(MemoryDetails*);
+	void initBufferData_B(VkCommandBuffer, VkQueue, MemInit);
+	std::vector<std::string> shaderFiles = { "shaders/cardRasterizer/01.spv", "shaders/cardRasterizer/02.spv" };
+
+
+private:
+	VkDevice device;
+	VkDescriptorPool descriptorPool;
+	VkRenderPass renderPass;
+
+	VkSampleCountFlagBits msaaSamples;
+
+	VkPhysicalDeviceMemoryProperties memProperties;
+
+	VkPipeline pipeline;
+	VkPipelineLayout pipelineLayout;
+	std::array<VkDescriptorSet, FRAMES_IN_FLIGHT> descriptorSets;
+	VkDescriptorSetLayout descriptorSetLayout;
+
+	float* aspectRatio;
+
+	VkRenderPass shadowPass;
+	VkPipeline shadowPipeline;
+	VkPipelineLayout shadowPipelineLayout;
+	std::array<VkDescriptorSet, FRAMES_IN_FLIGHT> shadowDescriptorSets;
+	VkDescriptorSetLayout shadowDescriptorSetLayout;
+
+	VkImage depthImage;
+	MemInit depthImageMemory;
+	VkImageView depthView;
+	VkFormat depthFormat = VK_FORMAT_D16_UNORM;
+
+	VkImage shadowImage;
+	MemInit shadowImageMemory;
+	VkSampler shadowSampler;
+	std::array<VkFramebuffer, 6> shadowFramebuffers;
+	std::array<VkImageView, 6> shadowCubeViews;
+	VkImageView shadowCubeMapView;
+	VkFormat shadowFormat = VK_FORMAT_R32_SFLOAT;
+	const uint32_t shadowImageSize = 1024;
+
+
+
+	VkBuffer vertexBuffer;
+	MemInit vertexMemory;
+	std::array < VkImage, 2>  texImage;
+	std::array < VkImageView, 2>  texImageView;
+	std::array < VkSampler, 2>  texSampler;
+	std::array<MemInit, 2> texMemory;
+	std::array<std::string, 2> texPaths = { "textures/Enhancers.png", "textures/8BitDeck.png" };
+	std::array<VkDeviceSize, 2> texSizes;
+
+	VkBuffer stagingBuffer;
+	MemInit stagingMemory;
+
+
+	VkBuffer uniformBuffer;
+	MemInit uniformBufferMemory;
+	char* uniformBufferMapped;
+	uint32_t uniformBufferSize;
+	uint32_t uniformBufferRegion;
+	std::array<char*, FRAMES_IN_FLIGHT> uniformsMapped;
+
+	std::vector<std::vector<playingCard>*>* hands;
+	std::vector<std::vector<playingCard>*>* wins;
+	std::vector<std::vector<playingCard>>* table;
+	std::vector<playingCard>* stock;
+
+	std::vector<glm::vec2> tablePositions{};
+	glm::vec2 tableOffset;
+	std::array<glm::vec2, 2> handPositons;
+	std::array<glm::vec2, 4> handOffsets;
+	glm::vec2 winOffset;
+	std::array<glm::vec2, 2> winPositions;
+	glm::vec2 stockOffset;
+	glm::vec2 stockPosition;
+	float cardHeightOffset;
+	float cardHeightZero;
+
+	PlayerObject* player;
+
+	std::array<CardDataConstant, CARD_COUNT> cardData;
+	std::array<glm::vec4, CARD_COUNT> currentCardData;
+	std::array<glm::vec4, CARD_COUNT> prevCardData;
+
+	std::chrono::time_point<std::chrono::high_resolution_clock> currentTime;
+	std::chrono::time_point<std::chrono::high_resolution_clock> commandSubmitTime = std::chrono::high_resolution_clock::now(); 
+
+	uint32_t frameCounter = 0;
+	uint32_t cardCounter = 0;
+	
+	std::array<std::vector<char>*, 2> shaderCode;
+
+	MemoryDetails vertexRequirements{};
+	MemoryDetails uniformRequirements{};
+	std::array<MemoryDetails, 2> texRequirements{};
+	MemoryDetails depthRequirements{};
+	MemoryDetails shadowRequirements{};
+
+	
+	void createPipeline();
+	void createDescriptorSets();
+	void createBuffers();
+	void createImageView();
+	void createSampler();
+	void createRenderPass();
+
+
+
+	std::vector<glm::vec4> vertices;
+
+
+};
+
---
+++ GravSim/shaders/cardRasterizer/02.frag
@@ -0,0 +1,101 @@
+#version 460
+
+layout(binding = 1) uniform sampler2D[2] texSampler;
+
+layout(location = 0) in vec2 cardBaseCoord;
+layout(location = 1) in vec2 cardBaseBlendCoord;
+layout(location = 2) in vec2 cardValCoord;
+layout(location = 3) in float blendConstant;
+layout(location = 4) flat in uint mode;
+layout(location = 5) in vec3 normal;
+layout(location = 6) in vec3 fragPos;
+
+layout(location = 0) out vec4 finalColour;
+
+layout(push_constant) uniform pc{
+    mat4 viewMat;
+    mat4 viewProjMat;
+    vec4 pos;
+    vec4 dir;
+    vec4 colour;
+    vec4 eyePos;
+};
+
+float cornerBlendRadius;
+float cardHeight;
+
+void main(){
+    vec4 outColour;
+    if (mode == 0){
+        outColour = texture(texSampler[0], cardBaseCoord);
+    }
+    else if (mode==1){
+        vec4 sampleColor = texture(texSampler[1], cardValCoord);
+        vec4 baseColorA = texture(texSampler[0], cardBaseCoord);
+        vec4 baseColorB = texture(texSampler[0], cardBaseBlendCoord);
+        vec4 baseColor = (1.0f - blendConstant) * baseColorA + blendConstant * baseColorB;
+
+
+        //outColour = sampleColor * sampleColor.w + vec4(colour, 1.0f) * (1-sampleColor.w);
+        //outColour = (sampleColor * sampleColor.a) + (vec4(colour, 1.0f) * 0.0f * (0.0f-sampleColor.a));
+        outColour = vec4(sampleColor.xyz * sampleColor.a + baseColor.xyz * (1.0f - sampleColor.a), baseColor.a);
+
+
+
+        //outColour = vec4(sampleColor.a, 0.0f, 0.0f, 1.0f);
+
+        //outColour = vec4(sampleColor.xyz, 1.0f);
+        //outColour = vec4(fragTexCoord.x, fragTexCoord.y, 0.0f, 1.0f);
+
+        //outColour = sampleColor;
+
+        //outColour = vec4(fragTexCoord, 0.0f, 1.0f);
+    }
+    else if(mode==2){
+        outColour = vec4(colour.xyz, 1.0f);
+    }
+    if(outColour.a == 0.0f){
+        discard;
+    }
+    //now we do lighting calculations;
+    vec3 fragEye = eyePos.xyz - fragPos.xyz;
+    vec3 lightFrag = fragPos.xyz - pos.xyz;
+
+    //diffuse
+    float diff = dot(normalize(lightFrag), normalize(normal));
+    
+    vec4 diffColour = outColour * max(diff, 0.0f);
+    
+    //ambient
+    float amb = 0.1f;
+    vec4 ambColour = outColour * amb;
+
+    //specular
+    vec3 halfwayVector = normalize(normalize(-fragEye) + normalize(lightFrag));
+    float spec = pow(dot(normalize(normal), halfwayVector), pos.a);
+    vec4 specColour = outColour * max(spec, 0.0f);
+
+    float ambCoeff = 1.0f;
+    float diffCoeff = 0.5f;
+    float specCoeff = 0.5f;
+
+    finalColour = (specCoeff * specColour + ambCoeff * ambColour + diffCoeff * diffColour);
+    
+    //finalColour = ambColour;
+    //finalColour = outColour * (amb + spec + diff);
+
+    finalColour.a = outColour.a;
+    //finalColour = vec4(normalize(lightFrag), 1.0f);
+    //finalColour = diffColour;
+    //finalColour = specColour;
+    //finalColour = vec4(halfwayVector, 1.0f);
+    //finalColour = vec4(normal, 1.0f);
+    //outColour = vec4(normal, 1.0f);
+
+
+
+    //outColour = vec4(outColour.x, outColour.y, 0.0, 1.0);
+    //outColour = vec4(fragTexCoord.x, 1.0 - fragTexCoord.y, 0.0, 1.0);
+    //outColour = vec4(fragTexCoord.x, fragTexCoord.y * 0, 0.0, 1.0);
+    //outColour = vec4(1.0, 1.0, 1.0, 0.0);
+}
