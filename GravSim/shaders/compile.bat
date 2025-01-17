a4b124dc65209ed799d1633a3287e06dd7b2ae4c
Gareth Everton <gareth.everton@outlook.com>
2025-01-17 15:31:38 +0000

    Update to VulkanSDK 1.4.304.0 2.0

--- GravSim/shaders/compile.bat
+++ GravSim/shaders/compile.bat
@@ -1,13 +1,13 @@
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe particleRasterizer\01_1.vert -o particleRasterizer\01.spv --target-env=vulkan1.3
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe particleRasterizer\02.frag -o particleRasterizer\02.spv --target-env=vulkan1.3
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe uiRasterizer\01.vert -o uiRasterizer\01.spv --target-env=vulkan1.3
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe uiRasterizer\02.frag -o uiRasterizer\02.spv --target-env=vulkan1.3
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe cardRasterizer\01.vert -o cardRasterizer\01.spv --target-env=vulkan1.3
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe cardRasterizer\02.frag -o cardRasterizer\02.spv --target-env=vulkan1.3
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe GravEngine\01.comp -o GravEngine\01.spv --target-env=vulkan1.3
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe GravEngine\SortShaders\06.comp -o GravEngine\SortShaders\01.spv --target-env=vulkan1.3
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe GravEngine\SortShaders\07.comp -o GravEngine\SortShaders\02.spv --target-env=vulkan1.3
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe GravEngine\SortShaders\08.comp -o GravEngine\SortShaders\03.spv --target-env=vulkan1.3
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe GravEngine\SortShaders\09.comp -o GravEngine\SortShaders\04.spv --target-env=vulkan1.3
-C:\VulkanSDK\1.3.296.0\Bin\glslc.exe GravEngine\SortShaders\10.comp -o GravEngine\SortShaders\05.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe particleRasterizer\01_1.vert -o particleRasterizer\01.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe particleRasterizer\02.frag -o particleRasterizer\02.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe uiRasterizer\01.vert -o uiRasterizer\01.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe uiRasterizer\02.frag -o uiRasterizer\02.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe cardRasterizer\01.vert -o cardRasterizer\01.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe cardRasterizer\02.frag -o cardRasterizer\02.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe GravEngine\01.comp -o GravEngine\01.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe GravEngine\SortShaders\06.comp -o GravEngine\SortShaders\01.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe GravEngine\SortShaders\07.comp -o GravEngine\SortShaders\02.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe GravEngine\SortShaders\08.comp -o GravEngine\SortShaders\03.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe GravEngine\SortShaders\09.comp -o GravEngine\SortShaders\04.spv --target-env=vulkan1.3
+C:\VulkanSDK\1.4.304.0\Bin\glslc.exe GravEngine\SortShaders\10.comp -o GravEngine\SortShaders\05.spv --target-env=vulkan1.3
 pause
